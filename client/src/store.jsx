9e773c3f656774c49f16fc105c677796d4e89bc9
gregory.gotlib <gregory.gotlib@gmail.com>
2019-02-11 20:40:16 +0200

    make it better2

--- client/src/store.jsx
+++ client/src/store.jsx
@@ -5,23 +5,20 @@ import rootReducer from './reducers';
 const initialState = {};
 const middleware = [thunk];
 
-if (process.env.NODE_ENV === "production") {
-    store = createStore(
-      rootReducer,
-      initialState,
-      compose(applyMiddleware(...middleware))
-    );
-  } 
-  else {
-    store = createStore(
+const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware)));
+
+/*
+   const store = createStore(
       rootReducer,
       initialState,
       compose(
         applyMiddleware(...middleware),
         window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__())
-    );
-  }
-
+          window.__REDUX_DEVTOOLS_EXTENSION__()));
+*/
 
 export default store; 
