c494df79d645cd7c65edec3b1b2b9615baccf2e6
gregory.gotlib <gregory.gotlib@gmail.com>
2019-02-11 19:03:24 +0200

    make it better

--- server.js
+++ server.js
@@ -32,19 +32,12 @@ app.use(express.static(uploadsDir));
 
 // Static files for prod
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/../../build')));
 
-    app.get('*', (request, response) => {
-        response.sendFile(path.join(__dirname + '/../../build/index.html'));
-        });
-
-    /*
     app.use(express.static('client/build'));
 
     app.get('*',(req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
-    */
 }
 
 
